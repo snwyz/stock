68867bd591ccfb0ea726817261b7acc3abed50bb
奉海龙 <feng@dongni.org>
2021-02-05 09:55:32 +0800

    第一次服务器上运行测试

--- hello.py
+++ hello.py
@@ -9,7 +9,7 @@ import schedule
 import hmac
 import hashlib
 import base64
-from apscheduler.schedulers.blocking import BlockingScheduler
+# from apscheduler.schedulers.blocking import BlockingScheduler
 
 # option = webdriver.ChromeOptions()
 # option.add_argument("headless")
@@ -28,7 +28,9 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 def getSign():
     timestamp = str(round(time.time() * 1000))
-    app_secret = 'SEC7ba3d34a066710739ea97a088ba30e0ae7cf6e3e82721451d54e3024fe501564'
+    # app_secret = 'SEC7ba3d34a066710739ea97a088ba30e0ae7cf6e3e82721451d54e3024fe501564'
+    app_secret = 'SECcf2a5f2524eea3527be0163eb36de8e1cfb2ecc6248890faa2c311ed32f456bb'
+
     app_secret_enc = app_secret.encode('utf-8')
     string_to_sign = '{}\n{}'.format(timestamp, app_secret)
     string_to_sign_enc = string_to_sign.encode('utf-8')
@@ -50,7 +52,7 @@ def getBoard():
     board_message = '大盘实时数据：' + '\n'
     for board_item in board_list:
         board_item['changePercent'] = board_item['changePercent'] + '%'
-        board_item['turnover'] = str(int(board_item['turnover']) / 10000) + '亿'
+        board_item['turnover'] = str('%.2f' % (int(board_item['turnover']) / 10000)) + '亿'
         message = board_item['name'] + '（' + board_item['changePercent'] + '）' + '：' \
                   + board_item['price'] + '，' + "成交金额：" + board_item['turnover'] + '\n'
         board_message = board_message + message
@@ -58,7 +60,7 @@ def getBoard():
     hot_stock_message = '个股成交量排行：' + '\n'
     for hot_stock_item in hot_stock_list:
         hot_stock_item['changePercent'] = hot_stock_item['changePercent'] + '%'
-        hot_stock_item['turnover'] = str(int(hot_stock_item['turnover']) / 10000) + '亿'
+        hot_stock_item['turnover'] = str('%.2f' % (int(hot_stock_item['turnover']) / 10000)) + '亿'
         message = hot_stock_item['name'] + '（' + hot_stock_item['changePercent'] + '）' + '：' \
                   + hot_stock_item['price'] + '，' + "成交金额：" + hot_stock_item['turnover'] + '\n'
         hot_stock_message = hot_stock_message + message
@@ -67,6 +69,9 @@ def getBoard():
 
 # 北向资金
 def get_realtime_money_flow():
+
+    current_time = time.strftime("%-H:%M", time.localtime())
+
     realtime_flow_url = 'http://push2.eastmoney.com/api/qt/kamt.rtmin/get?fields1=f1,f3&fields2=f51,f52,f54,f56'
     ret = requests.get(realtime_flow_url)
 
@@ -80,11 +85,8 @@ def get_realtime_money_flow():
     df = pd.DataFrame(map(lambda v: v.split(','), data['s2n']), columns=['time', 'hk2sh', 'hk2sz', 's2n']).\
         set_index('time').replace('-', np.nan).dropna().astype(float)
 
-    current_time = time.strftime("%H:%M", time.localtime())
-    if current_time != '15:00':
-        current_time = '15:00'
-    nouth_message = '北向资金实时流入：' + '\n' + '总流入：' + str(df.loc[current_time, 's2n'] / 10000) + '亿，' + '流向沪市：' \
-                    + str(df.loc[current_time, 'hk2sh'] / 10000) + '亿，' + '\n' + '流向深市：' + str(df.loc[current_time, 'hk2sz'] / 10000) + '亿'
+    nouth_message = '北向资金实时流入：' + '\n' + '总流入：' + str('%.2f' % (df.loc[current_time, 's2n'] / 10000)) + '亿，' + '流向沪市：' \
+                    + str('%.2f' % (df.loc[current_time, 'hk2sh'] / 10000)) + '亿，' + '\n' + '流向深市：' + str('%.2f' % (df.loc[current_time, 'hk2sz'] / 10000)) + '亿'
     postToDing(nouth_message)
 
 # 发送到钉钉
@@ -103,12 +105,12 @@ def postToDing(content):
             "isAtAll": False  # 是否要@某位用户
         }
     }
-    ding_url = 'https://oapi.dingtalk.com/robot/send?access_token=a6b488dc3f9881d3344f902a38fc78eeb74467b10dabbd0a16273eb3cb13fa97' + \
+    ding_url = 'https://oapi.dingtalk.com/robot/send?access_token=399aab2aeca9a235a1667a76eb2d202957685b3a47eaa6c3c0e089ba44860492' + \
                 '&timestamp=' + str(round(time.time() * 1000))+'&sign=' + sign
     requests.post(url=ding_url, json=json_data)
     print('报警信息发送成功。')
 
-schedule.every(1).minutes.do(get_realtime_money_flow)    # 每隔1分钟执行一次任务
+schedule.every(30).minutes.do(get_realtime_money_flow)    # 每隔1分钟执行一次任务
 schedule.every(5).minutes.do(getBoard)    # 每隔5分钟执行一次任务
 
 while True: 
